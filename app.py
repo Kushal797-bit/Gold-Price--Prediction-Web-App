dd0c80582faf0b200a1d206a263e645ff757991d
Kushal Upadhyay <kushalupadhyay06@example.com>
2026-05-31 13:56:41 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+# Load trained model
+model = pickle.load(open('model/model.pkl', 'rb'))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+
+    spx = float(request.form['SPX'])
+    uso = float(request.form['USO'])
+    slv = float(request.form['SLV'])
+    eurusd = float(request.form['EURUSD'])
+
+    features = np.array([[spx, uso, slv, eurusd]])
+
+    prediction = model.predict(features)
+
+    output = round(prediction[0], 2)
+
+    return render_template(
+        "index.html",
+        prediction_text=f"Predicted Gold Price: {output}"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
